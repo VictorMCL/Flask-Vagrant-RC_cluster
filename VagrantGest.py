57d3814e2c788dff1d821f9f6427ea05dd85132d
Victor_Lenovo <vict7422@gmail.com>
2019-06-25 21:23:22 -0500

    anadiendo logs

--- VagrantGest.py
+++ VagrantGest.py
@@ -4,6 +4,28 @@ import os
 import envConfig
 import subprocess
 import ManagementDB
+import logging
+
+# Create a custom logger
+logger = logging.getLogger(__name__)
+
+# Create handlers
+c_handler = logging.FileHandler(envConfig.FILELOG)
+f_handler = logging.FileHandler(envConfig.FILELOG)
+
+c_handler.setLevel(logging.WARNING)
+f_handler.setLevel(logging.ERROR)
+
+# Create formatters and add it to handlers
+c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+c_handler.setFormatter(c_format)
+f_handler.setFormatter(f_format)
+
+# Add handlers to the logger
+logger.addHandler(c_handler)
+logger.addHandler(f_handler)
 
 # Los posibles estados de una maquina son
 # not created, running, saved, poweroff
@@ -24,6 +46,9 @@ def VagrantStatus(NameProyect):
     os.chdir(envConfig.HOME)
     return VMs
 
+#-----------------------------------Gestion de VM-----------------------------------------
+
+# Este metodo es instanciado por medio de un Hilo.
 def VagrantUP(NameProyect, VM):
     os.chdir(envConfig.VAGRANTPROJECT+NameProyect)
     command = subprocess.Popen(["vagrant","up",VM],stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -38,6 +63,7 @@ def VagrantUP(NameProyect, VM):
 def VagrantHalt(NameProyect, VMs):
     os.chdir(envConfig.VAGRANTPROJECT+NameProyect)
     myCmd = os.popen("vagrant halt " + VMs).read()
+    ManagementDB.WriteElemt(NameProyect, VagrantStatus(NameProyect))
     os.chdir(envConfig.HOME)
     return myCmd
 
@@ -47,9 +73,12 @@ def VagrantDestroy(NameProyect, VMs):
         myCmd = os.popen("vagrant destroy -f").read()
     else:
         myCmd = os.popen("vagrant destroy " + VMs + " -f").read()
+    ManagementDB.WriteElemt(NameProyect, VagrantStatus(NameProyect))
     os.chdir(envConfig.HOME)
     # Organizar retorno de respuestas
     return myCmd
+
+#-----------------------------------Chequeo de VM-----------------------------------------
     
 def CheckVagrant(NameProyect):
     if os.path.isfile(envConfig.VAGRANTPROJECT+NameProyect+"/Vagrantfile") == True:
@@ -72,6 +101,8 @@ def VmCreated(NameProyect):
     os.chdir(envConfig.HOME)
     return lineas[0]
 
+#-----------------------------------General vagrant-----------------------------------------
+
 def VagrantVersion():
     myCmd = os.popen("vagrant version").read()
     return myCmd
@@ -92,10 +123,3 @@ def VagrantBoxRemove(VM):
     myCmd = os.popen("vagrant box remove " + VM).read()
     return myCmd
 
-
-#VagrantDestroy('ubuntu', '')
-#CheckVagrant("centos")
-#ManagementDB.WriteElemt('ubuntu', VagrantStatus('ubuntu'))
-#VmRunning('default')
-#VmNotCreates('default')
-#VagrantUP('default', 'node-1')
